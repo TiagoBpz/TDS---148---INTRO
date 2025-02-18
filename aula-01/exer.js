9db0d0a5bf3f69e2e2f446e2a18e5b8e4fc9090f
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2025-02-18 15:28:29 -0300

    elton

---
+++ aula-01/exer.js
@@ -0,0 +1,30 @@
+const idade = 18
+
+let Comidafavorita
+Comidafavorita = "Strogonoff"
+
+let celular
+console.log(celular)
+
+var carro, moto, tablet, carregador, relogio
+
+let nome = "Tiago"
+
+let Number
+number = "7"
+
+var name, sobrenome
+name = "Tiago"
+sobrenome = "Paulista"
+console.log("Meu nome é " + name + " " + sobrenome)
+console.log(`Meu nome é ${name} ${sobrenome}.`)
+
+let horario = "It´s time"
+console.log(horario)
+
+console.log(typeof idade, typeof nome, typeof sobrenome, typeof horario, typeof celular, typeof number)
+
+
+
+
+
---
+++ aula-01/script.js
@@ -0,0 +1,29 @@
+let nome = "Tiago"
+// string
+
+const idade = 18
+// Number
+
+var temCabeloComprido = "outra"
+var temCabeloComprido = "mais"
+var
+//booleon
+
+temCabeloComprido = "mais uma"
+// console.log(temCabeloComprido)
+
+var preco = 25
+var quantidade = 5
+var total = (preco * quantidade) - 10
+
+// console.log(total)
+
+// Concatenação
+var sobrenome, cidade, ano
+sobrenome = "Paulista"
+cidade = "Curitiba"
+ano = 2025
+
+console.log(nome, sobrenome, cidade)
+console.log("Meu nome é " + nome + " " + sobrenome + ", moro em" + cidade)
+console.log(`Meu nome é ${nome} ${sobrenome}, eu moro em ${cidade}.`)
