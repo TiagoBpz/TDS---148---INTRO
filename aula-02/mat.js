dc640b920eef9c37cd2f75e78bea26ad5e087b64
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2025-02-19 15:09:29 -0300

    aula02

--- aula-02/mat.js
+++ aula-02/mat.js
@@ -23,10 +23,17 @@ function Multiplicacao(){
     return res
 }
 function Divisao(){
+    document.getElementById("n8").style.border = "1px solid black"
     var numero7 = parseFloat(document.getElementById("n7").value)
     var numero8 = parseFloat(document.getElementById("n8").value)
     // console.log(typeof numero1, typeof numero2, numero1, numero2)
     var res = document.getElementById("resulta")
-    res.textContent = numero7 / numero8
+    if(numero8 !== 0){
+        res.textContent = (numero7 / numero8).toFixed(1)
+    }else{
+        res.innerHTML = "Não é permitido divisão por 0"
+        document.getElementById("n8").style.border = "2px solid red"
+    }
+    
     return res
 }
